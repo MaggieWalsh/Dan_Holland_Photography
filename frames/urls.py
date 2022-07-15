122cee0cb1ccde847e3261d9e6f9f565ecf7b7d5
Maggie Walsh <maggiewalsh@maggies-air.home>
2022-07-15 13:56:39 +0100

    Add frames and frames details pages and functionality

--- frames/urls.py
+++ frames/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_frames, name='frames'),
-    # path('<frame_id>', views.frame_detail, name='frame_detail'),
+    path('<frame_id>', views.frame_detail, name='frame_detail'),
 ]

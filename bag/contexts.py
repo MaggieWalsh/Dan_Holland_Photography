d13091a625fb2428a6432699bc7b080da3daf5a4
Maggie Walsh <maggiewalsh@maggies-air.home>
2022-07-17 21:57:54 +0100

    Update contexts.py with bag session to capture items added to basket

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,38 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+from frames.models import Frame
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    frame_count = 0
+
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    for item_id, quantity in bag.items():
+        frame = get_object_or_404(Frame, pk=item_id)
+        total += quantity * frame.price
+        frame_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'frame': frame,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

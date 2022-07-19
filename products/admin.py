8678e05a54625ec51b7763e13be022b5dec3a0b5
Maggie Walsh <maggiewalsh@maggies-air.home>
2022-07-19 22:06:11 +0100

    Remove images fixtures, model etc and reincorporate into products model. Change for loop on product details to drop the url in order to be able to view items with no imageFramed or imageWall attributes

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Image
+from .models import Product, Category
 
 # Register your models here.
 
@@ -21,11 +21,5 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-class ImageAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-    )
-
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Image, ImageAdmin)

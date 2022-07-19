6e27025f6e9f6649e1d7d4b0b3b29aff65bdefe9
Maggie Walsh <maggiewalsh@maggies-air.home>
2022-07-19 12:24:39 +0100

    Update models, fixtures and admin with new data

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Image
 
 # Register your models here.
 
@@ -11,8 +11,6 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
-        'imageFramed',
-        'imageWall',
     )
 
     ordering = ('sku',)
@@ -23,5 +21,11 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Image, ImageAdmin)

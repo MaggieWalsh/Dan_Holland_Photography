a01747da46732a64338644ea315e8feda7ea2960
Maggie Walsh <maggiewalsh@maggies-air.home>
2022-07-23 13:04:45 +0100

    Remove secret key and set debug

--- dan_holland_photography/settings.py
+++ dan_holland_photography/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asdfghjo987y6tredcvbnm'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dan-holland-photography.herokuapp.com', '127.0.0.1']
 
